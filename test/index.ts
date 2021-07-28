61c0fc900e315f15cedc42816c6e4aa33284763d
Alexey Berezin <beraliv.spb@gmail.com>
2021-07-28 12:45:03 +0200

    test: 🧪 testOpaque

--- test/index.ts
+++ test/index.ts
@@ -595,9 +595,15 @@ function testElementOf() {
   type a2 = Assert<IsExact<ElementOf<typeof t2>, "one">>;
 }
 
+// T = U
+type Assignable<T, U> = U extends T ? true : false;
+
 function testOpaque() {
-  type t1 = Assert<IsExact<Opaque<number, "a">, number & { __TYPE__: "a" }>>;
-  type t2 = Assert<IsExact<Opaque<"a", string>, never>>; // should blow on mismatched order
+  type t1 = Assert<IsExact<Assignable<number, Opaque<number, "a">>, true>>;
+  type t2 = Assert<IsExact<Assignable<Opaque<number, "a">, number>, false>>;
+  type t3 = Assert<IsExact<Assignable<Opaque<number, "a">, Opaque<number, "b">>, false>>;
+  type t4 = Assert<IsExact<Assignable<Opaque<number, "a">, Opaque<number, "a">>, true>>;
+  type t5 = Assert<IsExact<Opaque<"a", string>, never>>; // should blow on mismatched order
 }
 
 function testAsyncOrSyncType() {
