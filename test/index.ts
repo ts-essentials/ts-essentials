9f2be1db59c6c0bf1a711e5dbb82e1905e24148d
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-27 16:48:11 +0200

    test: 🧪 testWritable

--- test/index.ts
+++ test/index.ts
@@ -644,8 +644,10 @@ function testWritable() {
   type cases = [
     Assert<IsExact<Writable<{}>, {}>>,
     Assert<IsExact<Writable<{ readonly a: number }>, { a: number }>>,
+    Assert<IsExact<Writable<{ a: number }>, { a: number }>>,
     Assert<IsExact<Writable<[1, 2, 3]>, [1, 2, 3]>>,
     Assert<IsExact<Writable<readonly [1, 2, 3]>, [1, 2, 3]>>,
+    Assert<IsExact<Writable<number[]>, number[]>>,
     Assert<IsExact<Writable<readonly number[]>, number[]>>,
   ];
 }
