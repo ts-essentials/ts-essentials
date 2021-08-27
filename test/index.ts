196456f648b9870e4834c412b11375e9dee57cdc
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-27 17:23:15 +0200

    test: ➕ add more cases to testBuildable

--- test/index.ts
+++ test/index.ts
@@ -709,7 +709,46 @@ function testDeepWritable() {
 }
 
 function testBuildable() {
-  type Test = Assert<IsExact<Buildable<ComplexNestedReadonly>, ComplexNestedPartial>>;
+  type cases = [
+    Assert<IsExact<Buildable<number>, number>>,
+    Assert<IsExact<Buildable<string>, string>>,
+    Assert<IsExact<Buildable<boolean>, boolean>>,
+    Assert<IsExact<Buildable<bigint>, bigint>>,
+    Assert<IsExact<Buildable<symbol>, symbol>>,
+    Assert<IsExact<Buildable<undefined>, undefined>>,
+    Assert<IsExact<Buildable<null>, null>>,
+    Assert<IsExact<Buildable<Function>, Function>>,
+    Assert<IsExact<Buildable<Date>, Date>>,
+    Assert<IsExact<Buildable<Error>, Error>>,
+    Assert<IsExact<Buildable<RegExp>, RegExp>>,
+    Assert<IsExact<Buildable<Map<string, boolean>>, Map<string, boolean>>>,
+    Assert<IsExact<Buildable<ReadonlyMap<string, boolean>>, Map<string, boolean>>>,
+    Assert<IsExact<Buildable<WeakMap<{ key: string }, boolean>>, WeakMap<{ key: string }, boolean>>>,
+    Assert<
+      IsExact<Buildable<WeakMap<{ key: string }, { value: boolean }>>, WeakMap<{ key?: string }, { value?: boolean }>>
+    >,
+    Assert<IsExact<Buildable<Set<string>>, Set<string>>>,
+    Assert<IsExact<Buildable<ReadonlySet<string>>, Set<string>>>,
+    Assert<IsExact<Buildable<readonly []>, []>>,
+    Assert<IsExact<Buildable<readonly [1, 2, 3]>, [(1 | undefined)?, (2 | undefined)?, (3 | undefined)?]>>,
+    Assert<IsExact<Buildable<readonly number[]>, number[]>>,
+    Assert<IsExact<Buildable<ReadonlyArray<number>>, Array<number>>>,
+    Assert<IsExact<Buildable<Promise<number>>, Promise<number>>>,
+    Assert<IsExact<Buildable<{ readonly a: 1; readonly b: 2; readonly c: 3 }>, { a?: 1; b?: 2; c?: 3 }>>,
+    Assert<IsExact<Buildable<{ foo: () => void }>, { foo?: () => void }>>,
+    Assert<
+      IsExact<
+        Buildable<{ readonly obj: unknown; readonly arr: readonly unknown[] }>,
+        {
+          obj?: Partial<unknown> | undefined;
+          arr?: Partial<unknown>[] | undefined;
+        }
+      >
+    >,
+    Assert<IsExact<Buildable<DeepReadonly<ComplexNestedRequired>>, ComplexNestedPartial>>,
+    Assert<IsExact<Buildable<ComplexNestedRequired>, ComplexNestedPartial>>,
+    Assert<IsExact<Buildable<ComplexNestedReadonly>, ComplexNestedPartial>>,
+  ];
 }
 
 function testPickProperties() {
