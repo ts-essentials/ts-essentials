5b2455f7a97e4f21d870d91121e6bc98c01e4e37
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-28 08:59:53 +0200

    test: ➕ add wrong cases

--- test/index.ts
+++ test/index.ts
@@ -770,12 +770,25 @@ function testStrictExtract() {
   type Animal = Dog | Cat | Mouse;
 
   type cases = [
+    // @ts-expect-error
+    StrictExtract<Animal, undefined>,
+    Assert<IsExact<StrictExtract<Animal, never>, never>>,
     Assert<IsExact<StrictExtract<Animal, { type: undefined }>, never>>,
     Assert<IsExact<StrictExtract<Animal, { type: never }>, never>>,
+    // @ts-expect-error
+    StrictExtract<Animal, "dog">,
     Assert<IsExact<StrictExtract<Animal, { type: "dog" }>, Dog>>,
+    // @ts-expect-error
+    StrictExtract<Animal, "cat">,
     Assert<IsExact<StrictExtract<Animal, { type: "cat" }>, Cat>>,
+    // @ts-expect-error
+    StrictExtract<Animal, "mouse">,
     Assert<IsExact<StrictExtract<Animal, { type: "mouse" }>, Mouse>>,
+    // @ts-expect-error
+    StrictExtract<Animal, "cat" | "dog">,
     Assert<IsExact<StrictExtract<Animal, { type: "cat" | "dog" }>, Cat | Dog>>,
+    // @ts-expect-error
+    StrictExtract<Animal, "cat" | "dog" | "mouse">,
     Assert<IsExact<StrictExtract<Animal, { type: "cat" | "dog" | "mouse" }>, Animal>>,
   ];
 }
