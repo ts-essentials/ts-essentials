fd81822ec9653ac190f67d48d97984aebc2d2db6
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-30 19:43:28 +0200

    test: 🧪 testIsNever

--- test/index.ts
+++ test/index.ts
@@ -47,6 +47,7 @@ import {
   StrictOmit,
   OmitProperties,
   IsUnknown,
+  IsNever,
 } from "../lib";
 
 function testDictionary() {
@@ -1232,5 +1233,37 @@ function testIsUnknown() {
     Assert<IsExact<IsUnknown<unknown>, true>>,
     Assert<IsExact<IsUnknown<never>, false>>,
     Assert<IsExact<IsUnknown<any>, false>>,
+  ];
+}
+
+function testIsNever() {
+  type cases = [
+    Assert<IsExact<IsNever<string>, false>>,
+    Assert<IsExact<IsNever<number>, false>>,
+    Assert<IsExact<IsNever<boolean>, false>>,
+    Assert<IsExact<IsNever<bigint>, false>>,
+    Assert<IsExact<IsNever<symbol>, false>>,
+    Assert<IsExact<IsNever<undefined>, false>>,
+    Assert<IsExact<IsNever<null>, false>>,
+    Assert<IsExact<IsNever<Function>, false>>,
+    Assert<IsExact<IsNever<Date>, false>>,
+    Assert<IsExact<IsNever<Error>, false>>,
+    Assert<IsExact<IsNever<RegExp>, false>>,
+    Assert<IsExact<IsNever<Map<string, unknown>>, false>>,
+    Assert<IsExact<IsNever<ReadonlyMap<string, unknown>>, false>>,
+    Assert<IsExact<IsNever<WeakMap<{ a: 1 }, unknown>>, false>>,
+    Assert<IsExact<IsNever<Set<string>>, false>>,
+    Assert<IsExact<IsNever<ReadonlySet<string>>, false>>,
+    Assert<IsExact<IsNever<WeakSet<{ a: 1 }>>, false>>,
+    Assert<IsExact<IsNever<{ a: 1 }>, false>>,
+    Assert<IsExact<IsNever<[]>, false>>,
+    Assert<IsExact<IsNever<[1]>, false>>,
+    Assert<IsExact<IsNever<readonly [1]>, false>>,
+    Assert<IsExact<IsNever<readonly number[]>, false>>,
+    Assert<IsExact<IsNever<number[]>, false>>,
+    Assert<IsExact<IsNever<Promise<number>>, false>>,
+    Assert<IsExact<IsNever<unknown>, false>>,
+    Assert<IsExact<IsNever<never>, true>>,
+    Assert<IsExact<IsNever<any>, false>>,
   ];
 }
