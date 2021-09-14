e39426babeeeae0e3b33f4443c643cef3fa2719c
Alex Berezin <beraliv.spb@gmail.com>
2021-09-15 00:34:11 +0300

    feat: 🚀 IsUnknown (#252)
    * feat: 🚀 export IsUnknown
    
    * docs: 📄 changeset
    
    * test: 🧪 testIsUnknown
    
    * docs: ➕ add IsUnknown 📄
    
    * fix: 🧪 typo: never => unknown

--- test/index.ts
+++ test/index.ts
@@ -47,6 +47,7 @@ import {
   StrictOmit,
   OmitProperties,
   isExact,
+  IsUnknown,
 } from "../lib";
 
 function testDictionary() {
@@ -1258,4 +1259,36 @@ function testIsExact() {
   isBC(c);
   // @ts-expect-error has different structure from BC (missing property b)
   isBC(c2);
+}
+
+function testIsUnknown() {
+  type cases = [
+    Assert<IsExact<IsUnknown<string>, false>>,
+    Assert<IsExact<IsUnknown<number>, false>>,
+    Assert<IsExact<IsUnknown<boolean>, false>>,
+    Assert<IsExact<IsUnknown<bigint>, false>>,
+    Assert<IsExact<IsUnknown<symbol>, false>>,
+    Assert<IsExact<IsUnknown<undefined>, false>>,
+    Assert<IsExact<IsUnknown<null>, false>>,
+    Assert<IsExact<IsUnknown<Function>, false>>,
+    Assert<IsExact<IsUnknown<Date>, false>>,
+    Assert<IsExact<IsUnknown<Error>, false>>,
+    Assert<IsExact<IsUnknown<RegExp>, false>>,
+    Assert<IsExact<IsUnknown<Map<string, unknown>>, false>>,
+    Assert<IsExact<IsUnknown<ReadonlyMap<string, unknown>>, false>>,
+    Assert<IsExact<IsUnknown<WeakMap<{ a: 1 }, unknown>>, false>>,
+    Assert<IsExact<IsUnknown<Set<string>>, false>>,
+    Assert<IsExact<IsUnknown<ReadonlySet<string>>, false>>,
+    Assert<IsExact<IsUnknown<WeakSet<{ a: 1 }>>, false>>,
+    Assert<IsExact<IsUnknown<{ a: 1 }>, false>>,
+    Assert<IsExact<IsUnknown<[]>, false>>,
+    Assert<IsExact<IsUnknown<[1]>, false>>,
+    Assert<IsExact<IsUnknown<readonly [1]>, false>>,
+    Assert<IsExact<IsUnknown<readonly number[]>, false>>,
+    Assert<IsExact<IsUnknown<number[]>, false>>,
+    Assert<IsExact<IsUnknown<Promise<number>>, false>>,
+    Assert<IsExact<IsUnknown<unknown>, true>>,
+    Assert<IsExact<IsUnknown<never>, false>>,
+    Assert<IsExact<IsUnknown<any>, false>>,
+  ];
 }
