016871cec1707b98d578ddf9d5a60b5040bfa44f
Alexey Berezin <beraliv.spb@gmail.com>
2022-01-22 15:54:27 +0100

    test: ➕ add more cases with keys

--- test/mark-required.ts
+++ test/mark-required.ts
@@ -1,20 +1,26 @@
 import { AssertTrue as Assert, IsExact } from "conditional-type-checks";
-import { MarkRequired } from "../lib/types";
+import { MarkRequired, RequiredKeys, OptionalKeys } from "../lib/types";
 
 function testMarkRequired() {
   type Example = {
-    required: number;
+    readonly readonly1: Date;
+    readonly readonly2: RegExp;
+    required1: number;
     required2: string;
     optional1?: null;
     optional2?: boolean;
   };
 
   type cases = [
+    Assert<IsExact<MarkRequired<Example, RequiredKeys<Example>>, Example>>,
+    Assert<IsExact<MarkRequired<Example, OptionalKeys<Example>>, Required<Example>>>,
     Assert<
       IsExact<
-        MarkRequired<Example, "required2" | "optional1">,
+        MarkRequired<Example, "optional1">,
         {
-          required: number;
+          readonly readonly1: Date;
+          readonly readonly2: RegExp;
+          required1: number;
           required2: string;
           optional1: null;
           optional2?: boolean;
