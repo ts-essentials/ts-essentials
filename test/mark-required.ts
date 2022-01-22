57a098ea1c681852da5ea396d746897b2491aa60
Alexey Berezin <beraliv.spb@gmail.com>
2022-01-22 15:50:40 +0100

    test: ❌ remove ExpectedType

--- test/mark-required.ts
+++ test/mark-required.ts
@@ -2,18 +2,24 @@ import { AssertTrue as Assert, IsExact } from "conditional-type-checks";
 import { MarkRequired } from "../lib/types";
 
 function testMarkRequired() {
-  type TestType = {
-    required1: number;
+  type Example = {
+    required: number;
     required2: string;
     optional1?: null;
     optional2?: boolean;
   };
-  type ExpectedType = {
-    required1: number;
-    required2: string;
-    optional1: null;
-    optional2?: boolean;
-  };
 
-  type cases = [Assert<IsExact<MarkRequired<TestType, "required2" | "optional1">, ExpectedType>>];
+  type cases = [
+    Assert<
+      IsExact<
+        MarkRequired<Example, "required2" | "optional1">,
+        {
+          required: number;
+          required2: string;
+          optional1: null;
+          optional2?: boolean;
+        }
+      >
+    >,
+  ];
 }
