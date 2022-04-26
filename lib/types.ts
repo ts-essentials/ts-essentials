abfc09103546de9ba7700937c92895979669172e
psmolinsky <psmolinsky@gmail.com>
2022-04-26 10:39:28 +0200

    chore: fix styling according to prettier

--- lib/types.ts
+++ lib/types.ts
@@ -168,7 +168,7 @@ export type DeepReadonly<T> = T extends Builtin
   : T extends Promise<infer U>
   ? Promise<DeepReadonly<U>>
   : T extends AnyArray<infer U>
-  ? readonly DeepReadonly<U>[] 
+  ? readonly DeepReadonly<U>[]
   : T extends {}
   ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
   : IsUnknown<T> extends true
