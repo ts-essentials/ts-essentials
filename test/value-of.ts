c60291d2c06297986cb8b4cdd46b4490d7c8e247
ajitjha393 <ajitjha393@gmail.com>
2022-11-01 22:17:37 +0530

    Modify `ValueOf` to handle readonly arrays

--- test/value-of.ts
+++ test/value-of.ts
@@ -1,21 +1,35 @@
 ﻿import { AssertTrue as Assert, IsExact } from "conditional-type-checks";
-import { ValueOf, Primitive } from "../lib/types";
+import { ValueOf, Primitive, AnyArray } from "../lib/types";
 
-declare const array: number[] | undefined;
+declare const array: number[];
 declare const func: (...arg: any[]) => boolean;
 declare const obj: { propA: string; propB: number; propC: () => void };
-declare const tuple: [1, 2, 3, 4];
+declare const tuple: [boolean, string, number];
 declare const primitive: Primitive;
 declare const primitiveOrObject: Primitive | typeof obj;
 declare const objOrFunc: typeof func | typeof obj;
+declare const arrayOrUndefined: number[] | undefined;
+
+// declare const readonly
+declare const readonlyArray: AnyArray<string | number>;
+declare const readonlyArrayOrUndefined: AnyArray<string | number> | undefined;
+declare const readonlyTuple: readonly [boolean, string, number];
+declare const readonlyTupleOrUndefined: readonly [boolean, string, number] | undefined;
 
 function testValueOf() {
   type cases = [
     Assert<IsExact<ValueOf<typeof obj>, string | number | (() => void)>>,
-    Assert<IsExact<ValueOf<typeof array>, number | undefined>>,
+    Assert<IsExact<ValueOf<typeof array>, number>>,
+    Assert<IsExact<ValueOf<typeof arrayOrUndefined>, number | undefined>>,
+    Assert<IsExact<ValueOf<typeof readonlyArray>, string | number>>,
+    Assert<IsExact<ValueOf<typeof readonlyArrayOrUndefined>, string | number | undefined>>,
+    Assert<IsExact<ValueOf<typeof tuple>, boolean | string | number>>,
+    Assert<IsExact<ValueOf<typeof readonlyTuple>, boolean | string | number>>,
+    Assert<IsExact<ValueOf<typeof readonlyTupleOrUndefined>, boolean | string | number | undefined>>,
     Assert<IsExact<ValueOf<typeof func>, boolean>>,
-    Assert<IsExact<ValueOf<typeof tuple>, 1 | 2 | 3 | 4>>,
     Assert<IsExact<ValueOf<string>, string>>,
+    Assert<IsExact<ValueOf<Array<number | undefined>>, number | undefined>>,
+    Assert<IsExact<ValueOf<AnyArray<number | undefined>>, number | undefined>>,
     // valueOf for constant primitive literals will be the literal type
     Assert<IsExact<ValueOf<"const">, "const">>,
     Assert<IsExact<ValueOf<23>, 23>>,
@@ -25,7 +39,6 @@ function testValueOf() {
     Assert<IsExact<ValueOf<typeof primitive>, Primitive>>,
     Assert<IsExact<ValueOf<typeof primitiveOrObject>, ValueOf<Primitive> | ValueOf<typeof obj>>>,
     Assert<IsExact<ValueOf<typeof objOrFunc>, ValueOf<typeof obj | ValueOf<typeof func>>>>,
-    Assert<IsExact<ValueOf<23 | number>, number>>,
     Assert<IsExact<ValueOf<[number[], string[]]>, number[] | string[]>>,
     Assert<IsExact<ValueOf<number[] | string | null>, number | string | null>>,
     Assert<IsExact<ValueOf<boolean[] | typeof obj>, boolean | ValueOf<typeof obj>>>,
