7d963a1f61347ab76fa91c873c978d0852fe95aa
Alexey Berezin <beraliv.spb@gmail.com>
2023-04-02 22:25:45 +0100

    fix: 🧪 MarkRequired

--- lib/mark-required/index.ts
+++ lib/mark-required/index.ts
@@ -1,3 +1,1 @@
-export type MarkRequired<Type, Keys extends keyof Type> = Type extends Type
-  ? Omit<Type, Keys> & Required<Pick<Type, Keys>>
-  : never;
+export type MarkRequired<Type, Keys extends keyof Type> = Type extends Type ? Type & Required<Pick<Type, Keys>> : never;
