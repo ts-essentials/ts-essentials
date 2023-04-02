ef16a24f37a822c1f51b03b2253322dce39dafe3
Alexey Berezin <beraliv.spb@gmail.com>
2023-04-02 22:24:20 +0100

    test: ➕ add assignability checks

--- test/mark-readonly.ts
+++ test/mark-readonly.ts
@@ -1,19 +1,32 @@
 import { AssertTrue as Assert, IsExact } from "conditional-type-checks";
 import { MarkReadonly, WritableKeys, ReadonlyKeys } from "../lib";
+import { Debug } from "./types";
+
+type Example = {
+  readonly readonly1: Date;
+  readonly readonly2: RegExp;
+  required1: number;
+  required2: string;
+  optional1?: null;
+  optional2?: boolean;
+};
 
 function testMarkReadonly() {
-  type Example = {
-    readonly readonly1: Date;
-    readonly readonly2: RegExp;
-    required1: number;
-    required2: string;
-    optional1?: null;
-    optional2?: boolean;
-  };
+  type ExampleWithReadonlyRequired1 = Debug<MarkReadonly<Example, "required1">>;
 
-  type UnionExample = MarkReadonly<
-    Pick<Example, "readonly1" | "optional1"> | Pick<Example, "readonly2" | "optional1">,
-    "optional1"
+  type cases = [
+    Assert<IsExact<MarkReadonly<Example, never>, Example>>,
+    Assert<IsExact<MarkReadonly<Example, ReadonlyKeys<Example>>, Example>>,
+    Assert<IsExact<MarkReadonly<Example, WritableKeys<Example>>, Readonly<Example>>>,
+    Assert<IsExact<ReadonlyKeys<ExampleWithReadonlyRequired1>, "readonly1" | "readonly2" | "required1">>,
+    // @ts-expect-error do NOT support union types
+    MarkReadonly<Example | { a: 1 }, "required1">,
+  ];
+}
+
+function testUnionTypes() {
+  type UnionExample = Debug<
+    MarkReadonly<Pick<Example, "readonly1" | "optional1"> | Pick<Example, "readonly2" | "optional1">, "optional1">
   >;
 
   let unionElementFields: UnionExample = {
@@ -25,25 +38,27 @@ function testMarkReadonly() {
     readonly1: new Date(),
     optional1: null,
   };
+}
 
-  type cases = [
-    Assert<IsExact<MarkReadonly<Example, never>, Example>>,
-    Assert<IsExact<MarkReadonly<Example, ReadonlyKeys<Example>>, Example>>,
-    Assert<IsExact<MarkReadonly<Example, WritableKeys<Example>>, Readonly<Example>>>,
-    Assert<
-      IsExact<
-        MarkReadonly<Example, "required1">,
-        {
-          readonly readonly1: Date;
-          readonly readonly2: RegExp;
-          readonly required1: number;
-          required2: string;
-          optional1?: null;
-          optional2?: boolean;
-        }
-      >
-    >,
-    // @ts-expect-error do NOT support union types
-    MarkReadonly<Example | { a: 1 }, "required1">,
-  ];
+declare let example: Example;
+declare let readonlyExample: Readonly<Example>;
+declare let markedReadonlyExample: MarkReadonly<Example, "optional1">;
+
+function testAssignability() {
+  example = readonlyExample;
+  example = markedReadonlyExample;
+  readonlyExample = example;
+  markedReadonlyExample = example;
+
+  // it verifies that type `Readonly<Type>` is assignable to type `Type`
+
+  let assignabilityCheck1: <Type>(object: Type) => object is Readonly<Type>;
+
+  // it verifies that type `MarkReadonly<Type, PropertyName>`
+  // is assignable to type `Type`
+
+  let assignabilityCheck2: <Type, PropertyName extends keyof Type>(
+    object: Type,
+    propertyNames: PropertyName[],
+  ) => object is MarkReadonly<Type, PropertyName>;
 }
--- test/mark-writable.ts
+++ test/mark-writable.ts
@@ -1,31 +1,17 @@
 import { AssertTrue as Assert, IsExact } from "conditional-type-checks";
 import { MarkWritable, Writable, ReadonlyKeys, WritableKeys } from "../lib";
+import { Debug } from "./types";
+
+type Example = {
+  readonly readonly1: Date;
+  readonly readonly2: RegExp;
+  required1: number;
+  required2: string;
+  optional1?: null;
+  optional2?: boolean;
+};
 
 function testMarkWritable() {
-  type Example = {
-    readonly readonly1: Date;
-    readonly readonly2: RegExp;
-    required1: number;
-    required2: string;
-    optional1?: null;
-    optional2?: boolean;
-  };
-
-  type UnionExample = MarkWritable<
-    Pick<Example, "readonly1" | "optional1"> | Pick<Example, "readonly2" | "optional1">,
-    "optional1"
-  >;
-
-  let unionElementFields: UnionExample = {
-    readonly2: /\w+/g,
-    optional1: null,
-  };
-
-  unionElementFields = {
-    readonly1: new Date(),
-    optional1: null,
-  };
-
   type cases = [
     Assert<IsExact<MarkWritable<Example, never>, Example>>,
     Assert<IsExact<MarkWritable<Example, WritableKeys<Example>>, Example>>,
@@ -46,4 +32,43 @@ function testMarkWritable() {
     // @ts-expect-error do NOT support union types
     MarkWritable<Example | { a: 1 }, "readonly1">,
   ];
+}
+
+function testUnionTypes() {
+  type UnionExample = Debug<
+    MarkWritable<Pick<Example, "readonly1" | "optional1"> | Pick<Example, "readonly2" | "optional1">, "optional1">
+  >;
+
+  let unionElementFields: UnionExample = {
+    readonly2: /\w+/g,
+    optional1: null,
+  };
+
+  unionElementFields = {
+    readonly1: new Date(),
+    optional1: null,
+  };
+}
+
+declare let example: Example;
+declare let writableExample: Writable<Example>;
+declare let markedWritableExample: Debug<MarkWritable<Example, "readonly1">>;
+
+function testAssignability() {
+  example = writableExample;
+  example = markedWritableExample;
+  writableExample = example;
+  markedWritableExample = example;
+
+  // it verifies that type `Writable<Type>` is assignable to type `Type`
+
+  let assignabilityCheck1: <Type>(object: Type) => object is Writable<Type>;
+
+  // it verifies that type `MarkWritable<Type, PropertyName>`
+  // is assignable to type `Type`
+
+  let assignabilityCheck2: <Type, PropertyName extends keyof Type>(
+    object: Type,
+    propertyNames: PropertyName[],
+  ) => object is MarkWritable<Type, PropertyName>;
 }
