ee078260218a3252925f15a5098f79a207f02b3a
Alexey Berezin <beraliv.spb@gmail.com>
2024-04-17 00:32:03 +0100

    fix: 🧪 Paths

--- lib/paths/index.ts
+++ lib/paths/index.ts
@@ -1,32 +1,43 @@
-import { IsTuple } from "../is-tuple";
-import { Primitive } from "../primitive";
+import { AnyArray } from "../any-array";
+import { Builtin } from "../built-in";
+import { IsNever } from "../is-never";
 
-type Pathable = string | number;
+type Pathable = string | bigint | number;
 
-type Join<TKey extends Pathable, TPath> = TPath extends Pathable
-  ? `${TKey}${"" extends TPath ? "" : "."}${TPath}`
-  : never;
+type UnsafeValue<Type, Key> = Key extends keyof Type ? Type[Key] : never;
 
-type PreviousIndexMapping = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
+// Prevent inference of non-recursive type methods, e.g. Promise.then or Array.entries
+type NonRecursiveType = Builtin | Promise<unknown> | ReadonlyMap<unknown, unknown> | ReadonlySet<unknown>;
 
-type TupleKeys<TTuple extends readonly any[]> = Exclude<keyof TTuple, keyof any[]>;
+type Stringify<T> = T extends Pathable ? `${T}` : never;
 
-type PathEvaluation<TKey extends Pathable, TValue, TDepth extends number> = TValue extends Primitive
-  ? `${TKey}`
-  : `${TKey}` | Join<TKey, Paths<TValue, PreviousIndexMapping[TDepth]>>;
+// Convert numeric indices to bigint to prevent unintentional float indices
+type StrictIndex<Key extends Pathable> = Key extends number ? bigint : Key;
 
-type Paths<Type, TDepth extends number = 5> = [TDepth] extends [never]
+// Caching `RecursivePaths = Paths<UnsafeValue<Type, Key>>` to prevent double calculation
+type RecursivePaths<
+  Type,
+  Key extends Pathable,
+  RecursivePaths = Paths<UnsafeValue<Type, Key>>,
+> = IsNever<RecursivePaths> extends false ? `${StrictIndex<Key>}.${Stringify<RecursivePaths>}` : never;
+
+type UnsafePaths<Type> = Type extends readonly []
   ? never
-  : Type extends Array<infer Values>
-  ? Type extends IsTuple<Type>
-    ? {
-        [TKey in TupleKeys<Type>]-?: PathEvaluation<TKey & Pathable, Type[TKey], TDepth>;
-      }[TupleKeys<Type>]
-    : `${bigint}` | Join<`${bigint}`, Paths<Values, PreviousIndexMapping[TDepth]>>
+  : {
+      [Key in keyof Type]: Key extends Pathable
+        ? StrictIndex<Key> | `${StrictIndex<Key>}` | RecursivePaths<Type, Key>
+        : never;
+    }[Type extends AnyArray
+      ? // only numeric keys are acceptable for arrays/tuples
+        number & keyof Type
+      : keyof Type];
+
+type Paths<Type> = Type extends NonRecursiveType
+  ? never
+  : Type extends AnyArray
+  ? UnsafePaths<Type>
   : Type extends object
-  ? {
-      [TKey in keyof Type]-?: PathEvaluation<TKey & Pathable, Type[TKey], TDepth>;
-    }[keyof Type]
-  : "";
+  ? UnsafePaths<Type>
+  : never;
 
 export { Paths };
