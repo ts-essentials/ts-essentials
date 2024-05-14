c30388930a39ce2bf7d3eb913527e7615bbb18aa
Alexey Berezin <beraliv.spb@gmail.com>
2024-05-14 23:01:02 +0100

    ci: 🧪 bench

--- bench/example.ts
+++ bench/example.ts
@@ -10,7 +10,7 @@ type makeComplexType<s extends string> = s extends `${infer head}${infer tail}`
 bench("bench type", () => {
   return {} as makeComplexType<"defenestration">;
   // This is an inline snapshot that will be populated or compared when you run the file
-}).types([169, "instantiations"]);
+}).types([177, "instantiations"]);
 
 bench("bench runtime and type", () => {
   return {} as makeComplexType<"antidisestablishmentarianism">;
@@ -18,4 +18,4 @@ bench("bench runtime and type", () => {
   // Average time it takes the function execute
   .mean([2, "ms"])
   // Seems like our type is O(n) with respect to the length of the input- not bad!
-  .types([337, "instantiations"]);
+  .types([345, "instantiations"]);
