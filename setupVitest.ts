7a48f9bd84c958e9369f6796d5439e680456c97f
Alexey Berezin <beraliv.spb@gmail.com>
2024-05-14 23:08:42 +0100

    ❌ remove vitest

--- setupVitest.ts
+++
@@ -1,5 +0,0 @@
-import { setup as attestSetup, teardown as attestTeardown } from "@arktype/attest";
-
-export const setup = () => attestSetup({});
-
-export const teardown = attestTeardown;
--- vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globalSetup: ["setupVitest.ts"],
-    include: ["bench/**/*.ts"],
-  },
-});
