39eb4244d890441fe504c51ca2d89f18d36405ea
Alexey Berezin <2991847+Beraliv@users.noreply.github.com>
2024-06-19 11:13:57 +0100

    Fix: Remove XOR union element with all properties excluded from the intersection (#401)
    * test: pre-v10 implementation
    
    * test: more cases for XOR
    
    * fix: remove union element with all properties excluded from the intersection
    
    * docs: changelog

--- test/xor.ts
+++ test/xor.ts
@@ -1,23 +1,26 @@
 import { AssertTrue as Assert, IsExact } from "conditional-type-checks";
-import { XOR } from "../lib";
+import { isExact, XOR } from "../lib";
 
-function testXOR() {
+function testXOR2InTypes() {
   type TestType1 = { a: string };
   type TestType2 = { a: number; b: boolean };
   type TestType3 = { c: number; d: boolean };
 
   type Actual1 = XOR<TestType1, TestType2>;
-  type Expected1 = { a: string; b?: never } | { a: number; b: boolean } | { a?: never; b?: never };
+  type Expected1 = { a: string; b?: never } | { a: number; b: boolean };
 
   type Actual2 = XOR<TestType1, TestType3>;
-  type Expected2 =
-    | { a: string; c?: never; d?: never }
-    | { a?: never; c: number; d: boolean }
-    | { a?: never; c?: never; d?: never };
+  type Expected2 = { a: string; c?: never; d?: never } | { a?: never; c: number; d: boolean };
+
+  type Actual3 = XOR<TestType2, TestType3>;
+  type Expected3 = { a: number; b: boolean; c?: never; d?: never } | { a?: never; b?: never; c: number; d: boolean };
 
   type Test1 = Assert<IsExact<Actual1, Expected1>>;
   type Test2 = Assert<IsExact<Actual2, Expected2>>;
+  type Test3 = Assert<IsExact<Actual3, Expected3>>;
+}
 
+function testXOR8() {
   type SinglePet = XOR<
     { cat: string },
     { dog: string },
@@ -34,6 +37,129 @@ function testXOR() {
   singlePet = { dog: "Sirius" };
   // @ts-expect-error: cannot have both
   singlePet = { cat: "Timofey", dog: "Sirius" };
+}
+
+const assertUndefined = isExact<undefined>();
+const assertNumber = isExact<number>();
+const assertNumberOrUndefined = isExact<number | undefined>();
+const assertString = isExact<string>();
+const assertStringOrUndefined = isExact<string | undefined>();
+const assertStringOrNumber = isExact<string | number>();
+const assertBoolean = isExact<boolean>();
+const assertBooleanOrUndefined = isExact<boolean | undefined>();
+declare const booleanToString: (_: boolean) => string;
+declare const numberToString: (_: number) => string;
+
+function testXORInFunctions() {
+  // Types have to have mutually exclusive properties
+
+  type TestType1 = { a: string };
+  type TestType2 = { b: boolean };
+  type TestType3 = { c: number };
+
+  const test1_1 = ({ a, b }: XOR<TestType1, TestType2>): string => {
+    if (typeof a === "string") return a;
+    assertUndefined(a);
+    assertBoolean(b);
+    return booleanToString(b);
+  };
+
+  const test1_2 = ({ a, b }: XOR<TestType1, TestType2>): string => {
+    if (typeof b === "boolean") return booleanToString(b);
+    assertString(a);
+    assertUndefined(b);
+    return a;
+  };
+
+  const test2_1 = ({ a, b }: XOR<TestType1, TestType2, unknown>): string => {
+    if (typeof a === "string") return a;
+    assertUndefined(a);
+    assertBoolean(b);
+    return booleanToString(b);
+  };
+
+  const test2_2 = ({ a, b }: XOR<TestType1, TestType2, unknown>): string => {
+    if (typeof b === "boolean") return booleanToString(b);
+    assertString(a);
+    assertUndefined(b);
+    return a;
+  };
+
+  const test3_1 = ({ a, b, c }: XOR<TestType1, TestType2, TestType3>): string => {
+    if (typeof a === "string") return a;
+    assertUndefined(a);
+    assertBooleanOrUndefined(b);
+    assertNumberOrUndefined(c);
+    if (typeof b === "boolean") return booleanToString(b);
+    assertUndefined(a);
+    assertUndefined(b);
+    assertNumber(c);
+    return numberToString(c);
+  };
+
+  const test3_2 = ({ a, b, c }: XOR<TestType1, TestType2, TestType3>): string => {
+    if (typeof b === "boolean") return booleanToString(b);
+    assertStringOrUndefined(a);
+    assertUndefined(b);
+    assertNumberOrUndefined(c);
+    if (typeof a === "string") return a;
+    assertUndefined(a);
+    assertUndefined(b);
+    assertNumber(c);
+    return numberToString(c);
+  };
+
+  const test3_3 = ({ a, b, c }: XOR<TestType1, TestType2, TestType3>): string => {
+    if (typeof c === "number") return numberToString(c);
+    assertStringOrUndefined(a);
+    assertBooleanOrUndefined(b);
+    assertUndefined(c);
+    if (typeof a === "string") return a;
+    assertUndefined(a);
+    assertBoolean(b);
+    assertUndefined(c);
+    return booleanToString(b);
+  };
+
+  // These types don't have to have mutually exclusive properties
+
+  type TestType4 = { d: string };
+  type TestType5 = { d: number; e: string };
+  type TestType6 = { e: number; f: boolean };
+
+  const test4 = ({ d, e }: XOR<TestType4, TestType5>): string => {
+    if (typeof d === "string") return d;
+    assertNumber(d);
+    assertStringOrUndefined(e);
+    if (typeof e === "string") return e;
+    assertUndefined(d);
+    assertUndefined(e);
+    return "";
+  };
+
+  const test5_1 = ({ d, e, f }: XOR<TestType4, TestType5, TestType6>): string => {
+    if (typeof d === "string") return d;
+    assertNumberOrUndefined(d);
+    assertStringOrNumber(e);
+    assertBooleanOrUndefined(f);
+    if (typeof e === "string") return e;
+    assertUndefined(d);
+    assertNumber(e);
+    assertBoolean(f);
+    return `${numberToString(e)},${booleanToString(f)}`;
+  };
+
+  const test5_2 = ({ d, e, f }: XOR<TestType4, TestType5, TestType6>): string => {
+    if (typeof e === "string") return e;
+    assertStringOrUndefined(d);
+    assertNumberOrUndefined(e);
+    assertBooleanOrUndefined(f);
+    if (typeof d === "string") return d;
+    assertUndefined(d);
+    assertNumber(e);
+    assertBoolean(f);
+    return `${numberToString(e)},${booleanToString(f)}`;
+  };
 }
 
 function testVariadicXOR() {
@@ -54,7 +180,7 @@ function testVariadicXOR() {
   type Test = Assert<IsExact<Type1, Type2>>;
 }
 
-function stressTestXOR() {
+function testXOR50Assignability() {
   type StressTest = XOR<
     { property1: string },
     { property2: string },
