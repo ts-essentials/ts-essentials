b7b546ca8a2cf7fa4baff28ab8c1d5864e1cb4a1
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-10-23 18:17:05 +0530

    refactor: implemention of OptionalKeys

--- lib/optional-keys/index.ts
+++ lib/optional-keys/index.ts
@@ -1,5 +1,5 @@
 export type OptionalKeys<Type> = Type extends object
   ? {
-      [Key in keyof Type]-?: undefined extends { [Key2 in keyof Type]: never }[Key] ? Key : never;
+      [Key in keyof Type]-?: Type extends Required<Pick<Type, Key>> ? never : Key;
     }[keyof Type & (Type extends ReadonlyArray<any> ? number : keyof Type)]
   : never;
