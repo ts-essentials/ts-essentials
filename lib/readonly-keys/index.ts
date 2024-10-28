c3d276b956814214c51136e5a498101fe55f18cd
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-10-28 11:04:40 +0530

    fix: remove special handling for arrays

--- lib/readonly-keys/index.ts
+++ lib/readonly-keys/index.ts
@@ -4,6 +4,5 @@ import { IsTuple } from "../is-tuple";
 export type ReadonlyKeys<Type extends object> = Type extends unknown
   ? keyof {
       [Key in keyof Type as IsFullyWritable<Pick<Type, Key>> extends true ? never : Key]: any;
-    } &
-      (Type extends ReadonlyArray<any> ? (Type extends IsTuple<Type> ? `${number}` : number) : unknown)
+    }
   : never;
