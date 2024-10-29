31238d41d48d26393236aa190a0a28e9f4a932e9
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-10-29 15:27:46 +0530

    refactor: switch to never from any

--- lib/optional-keys/index.ts
+++ lib/optional-keys/index.ts
@@ -1,5 +1,5 @@
 export type OptionalKeys<Type> = Type extends object
   ? keyof {
-      [Key in keyof Type as Type extends Required<Pick<Type, Key>> ? never : Key]: any;
+      [Key in keyof Type as Type extends Required<Pick<Type, Key>> ? never : Key]: never;
     }
   : never;
--- lib/readonly-keys/index.ts
+++ lib/readonly-keys/index.ts
@@ -2,6 +2,6 @@ import { IsFullyWritable } from "../is-fully-writable";
 
 export type ReadonlyKeys<Type extends object> = Type extends unknown
   ? keyof {
-      [Key in keyof Type as IsFullyWritable<Pick<Type, Key>> extends true ? never : Key]: any;
+      [Key in keyof Type as IsFullyWritable<Pick<Type, Key>> extends true ? never : Key]: never;
     }
   : never;
