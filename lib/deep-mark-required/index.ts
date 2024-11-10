ca6055ce581992beb5d07a94011426aeec437ee7
Alexey Berezin <beraliv.spb@gmail.com>
2024-11-10 18:48:59 +0000

    feat: RecursiveDeepMarkRequiredSinglePath supports unions and null on objects

--- lib/deep-mark-required/index.ts
+++ lib/deep-mark-required/index.ts
@@ -1,18 +1,17 @@
-import { AreNonDistributiveEqual } from "../are-non-distributive-equal";
 import { HasParsablePath } from "../has-parsable-path";
 import { MarkRequired } from "../mark-required";
 import { Paths } from "../paths";
 import { Prettify } from "../prettify";
 import { UnionToTuple } from "../union-to-tuple";
 
-type RecursiveDeepMarkRequiredSinglePath<Type, KeyPath> = KeyPath extends `${infer PropertyKey}.${infer RestKeyPath}`
-  ? {
-      [Key in keyof Type]: AreNonDistributiveEqual<Key, PropertyKey> extends true
-        ? Prettify<RecursiveDeepMarkRequiredSinglePath<NonNullable<Type[Key]>, RestKeyPath>>
-        : Type[Key];
-    }
-  : KeyPath extends keyof Type
-  ? Prettify<MarkRequired<Type, KeyPath>>
+type RecursiveDeepMarkRequiredSinglePath<Type, KeyPath> = Type extends object
+  ? KeyPath extends `${infer PropertyKey}.${infer RestKeyPath}`
+    ? {
+        [Key in keyof Type]: Key extends PropertyKey
+          ? Prettify<RecursiveDeepMarkRequiredSinglePath<Type[Key], RestKeyPath>>
+          : Type[Key];
+      }
+    : Prettify<MarkRequired<Type, KeyPath & keyof Type>>
   : Type;
 
 type RecursiveDeepMarkRequired<Accumulator, KeyPaths extends string[]> = KeyPaths extends [
