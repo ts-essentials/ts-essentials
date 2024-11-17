0919d4cd132e38bb367ff10766684b6222825b65
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-11-18 00:47:59 +0530

    refactor: replace Pick and Exclude with Omit

--- lib/mark-optional/index.ts
+++ lib/mark-optional/index.ts
@@ -1,5 +1,5 @@
 import { OptionalKeys } from "../optional-keys";
 
 export type MarkOptional<Type, Keys extends keyof Type> = Type extends Type
-  ? Partial<Type> & Required<Pick<Type, Exclude<keyof Type, Keys | OptionalKeys<Type & object>>>>
+  ? Partial<Type> & Required<Omit<Type, Keys | OptionalKeys<Type & object>>>
   : never;
--- lib/mark-readonly/index.ts
+++ lib/mark-readonly/index.ts
@@ -2,6 +2,5 @@ import { ReadonlyKeys } from "../readonly-keys";
 import { Writable } from "../writable";
 
 export type MarkReadonly<Type, Keys extends keyof Type> = Type extends Type
-  ? Readonly<Type> &
-      Writable<Pick<Type, Exclude<keyof Type, Keys | (Type extends object ? ReadonlyKeys<Type> : never)>>>
+  ? Readonly<Type> & Writable<Omit<Type, Keys | ReadonlyKeys<Type & object>>>
   : never;
