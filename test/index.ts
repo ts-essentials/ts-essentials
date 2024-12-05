ecb490bac5078720ce336a2d0ac63e6f4b6584f9
Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>
2024-12-06 02:45:36 +0530

    Fix: `Head` type with union of empty and non-empty tuple (#425)
    * fix: tail with optional, readonly and non-tuple arrays
    
    * add: changeset
    
    * chore: improve test
    
    * test: union of tuples with different lengths
    
    * fix: head with union of empty tuple and non-empty tuple
    
    * chore: add changeset

--- test/index.ts
+++ test/index.ts
@@ -626,10 +626,22 @@ function testNoop() {
 }
 
 function testHead() {
-  type List1 = [number, string, boolean, "a" | "b"];
-
-  type TestHead = Assert<IsExact<Head<List1>, number>>;
-  type TestHeadOnEmpty = Assert<IsExact<Head<[]>, never>>;
+  type cases = [
+    Assert<IsExact<Head<[]>, never>>,
+    Assert<IsExact<Head<readonly []>, never>>,
+    Assert<IsExact<Head<[1 | 2, 2, 3]>, 1 | 2>>,
+    Assert<IsExact<Head<readonly [1 | 2, 2, 3]>, 1 | 2>>,
+    Assert<IsExact<Head<[1]>, 1>>,
+    Assert<IsExact<Head<[1?]>, 1 | undefined>>,
+    Assert<IsExact<Head<[1, 2] | [3, 4, 5]>, 1 | 3>>,
+    Assert<IsExact<Head<[1, 2] | []>, 1>>,
+    Assert<IsExact<Head<[1?, 2?, 3?]>, 1 | undefined>>,
+    Assert<IsExact<Head<[...number[], 1, 2]>, number>>,
+    Assert<IsExact<Head<number[]>, number>>,
+    Assert<IsExact<Head<any[]>, any>>,
+    Assert<IsExact<Head<any>, any>>,
+    Assert<IsExact<Head<never>, never>>,
+  ];
 }
 
 function testTail() {
