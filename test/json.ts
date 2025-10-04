389b7d2ec82bad4531bbd566b4cb3c21cfd07d29
Alexey Berezin <beraliv.spb@gmail.com>
2025-10-04 14:24:12 +0100

    test: testJsonPrimitive

---
+++ test/json.ts
@@ -0,0 +1,87 @@
+import { JsonPrimitive, JsonValue } from "../lib";
+
+function testJsonPrimitive() {
+  let primitive: JsonPrimitive;
+
+  primitive = 1;
+  primitive = "1";
+  primitive = true;
+  // @ts-expect-error Type 'unique symbol' is not assignable to type 'JsonPrimitive'
+  primitive = Symbol.iterator;
+  primitive = null;
+  // @ts-expect-error Type 'undefined' is not assignable to type 'JsonPrimitive'
+  primitive = undefined;
+}
+
+function testCompatibleJson() {
+  type JsonCompatible<T> = {
+    [P in keyof T]: T[P] extends JsonValue
+      ? T[P]
+      : Pick<T, P> extends Required<Pick<T, P>>
+      ? never
+      : T[P] extends (() => any) | undefined
+      ? never
+      : JsonCompatible<T[P]>;
+  };
+
+  function test<T extends JsonCompatible<T>>(_: T): T {
+    return null as any;
+  }
+
+  interface RequiredNumberObject {
+    a: number;
+  }
+
+  class RequiredNumberClass {
+    a!: number;
+  }
+
+  interface OptionalNumberObject {
+    a?: number;
+  }
+
+  interface RequiredDateObject {
+    a: Date;
+  }
+
+  interface OptionalDateObject {
+    a?: Date;
+  }
+
+  interface NumberOrUndefinedObject {
+    a: number | undefined;
+  }
+
+  interface OptionalAnyFunctionObject {
+    a?: () => any;
+  }
+
+  test(null);
+  test(false);
+  test(0);
+  test("");
+  test([]);
+  test({});
+  test([0]);
+  test({ a: 0 });
+  test({} as RequiredNumberObject);
+  test({} as RequiredNumberClass);
+  test({} as OptionalNumberObject);
+
+  // @ts-expect-error Type '() => string' is not assignable to type 'never'
+  test(new Date());
+  // @ts-expect-error Type 'Date' is not assignable to type 'never'
+  test([new Date()]);
+  // @ts-expect-error Type 'Date' is not assignable to type 'never'
+  test({ a: new Date() });
+  // @ts-expect-error Type 'undefined' is not assignable to type 'never'
+  test({ a: undefined });
+  // @ts-expect-error  Type 'Date' is not assignable to type 'never'
+  test({} as RequiredDateObject);
+  // @ts-expect-error  Type '() => string' is not assignable to type 'never'
+  test({} as OptionalDateObject);
+  // @ts-expect-error Type 'undefined' is not assignable to type 'never'
+  test({} as NumberOrUndefinedObject);
+  // @ts-expect-error Type '() => any' is not assignable to type 'undefined'
+  test({} as OptionalAnyFunctionObject);
+}
