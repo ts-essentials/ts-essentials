fe22b7a49841cecd4dbe29b582d1b04767c5d22d
Alexey Berezin <beraliv.spb@gmail.com>
2025-10-06 23:57:57 +0100

    docs: credits to som-sm

--- lib/union-keys/index.ts
+++ lib/union-keys/index.ts
@@ -1,6 +1,6 @@
 import { UnionToIntersection } from "../union-to-intersection";
 
-// Adopted a hack from type-fest - https://github.com/sindresorhus/type-fest/pull/1009
+// Adopted a workaround by som-sm from type-fest - https://github.com/sindresorhus/type-fest/pull/1009
 export type UnionKeys<UnionType> = keyof UnionToIntersection<
   UnionType extends UnionType ? Record<keyof UnionType, never> : never
 >;
