6eb85010c93d5d923a452592750a14654ebe2460
Alexey Berezin <beraliv.spb@gmail.com>
2025-10-06 22:48:41 +0100

    feat: RequireAtLeastOne

---
+++ lib/require-at-least-one/index.ts
@@ -0,0 +1,12 @@
+import { AnyRecord } from "../any-record";
+import { IsAny } from "../is-any";
+import { StrictOmit } from "../strict-omit";
+
+export type _RequireAtLeastOne<Type extends AnyRecord, Keys extends keyof Type = keyof Type> = {
+  [Key in Keys]: Required<Pick<Type, Key>>;
+}[Keys] &
+  StrictOmit<Type, Keys>;
+
+export type RequireAtLeastOne<Type extends AnyRecord, Keys extends keyof Type = keyof Type> = IsAny<Type> extends true
+  ? any
+  : _RequireAtLeastOne<Type, Keys>;
