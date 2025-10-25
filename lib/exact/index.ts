df5baafb97c06508db73344846010c96f6130546
kadhirr <53949365+kadhirr@users.noreply.github.com>
2025-10-25 20:08:09 +0530

    chore: fix prettier issues

--- lib/exact/index.ts
+++ lib/exact/index.ts
@@ -1,7 +1,6 @@
 import { IsExact } from "../is-exact";
 
-
 /**
  * @deprecated `Exact` will be removed in v11.0. Use `IsExact<TValue,TShape>`
  */
-export type Exact<TValue, TShape> = IsExact<TValue,TShape>
+export type Exact<TValue, TShape> = IsExact<TValue, TShape>;
