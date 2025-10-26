30a312882bdecd107d350fa51d3020a1a47415fe
Kadhirravan R <53949365+kadhirr@users.noreply.github.com>
2025-10-26 10:11:20 +0530

    chore: update individual doc of exact
    Co-authored-by: Alexey Berezin <2991847+Beraliv@users.noreply.github.com>

--- lib/exact/index.ts
+++ lib/exact/index.ts
@@ -1,6 +1,6 @@
 import { IsExact } from "../is-exact";
 
 /**
- * @deprecated `Exact` will be removed in v11.0. Use `IsExact<TValue,TShape>`
+ * @deprecated `Exact` will be removed in v11.0. Use `IsExact<TValue, TShape>` instead
  */
 export type Exact<TValue, TShape> = IsExact<TValue, TShape>;
