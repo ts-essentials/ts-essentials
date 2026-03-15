6f88f6ed4394ddde79abf6efdae5ddf096ceb8d7
Alexey Berezin <beraliv.spb@gmail.com>
2026-03-15 21:57:09 +0000

    test: compact assertions in testElementOf

--- lib/element-of/index.test.ts
+++ lib/element-of/index.test.ts
@@ -3,7 +3,10 @@ import { ElementOf } from "..";
 
 function testElementOf() {
   const t1 = [1, 2, true, false];
-  type a1 = Assert<IsExact<ElementOf<typeof t1>, number | boolean>>;
   const t2 = ["one"] as const;
-  type a2 = Assert<IsExact<ElementOf<typeof t2>, "one">>;
+
+  type assertions = [
+    Assert<IsExact<ElementOf<typeof t1>, number | boolean>>,
+    Assert<IsExact<ElementOf<typeof t2>, "one">>,
+  ];
 }
