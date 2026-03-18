bb7d19fe4754a6809267c2ba355500f0ea386573
Alexey Berezin <2991847+Beraliv@users.noreply.github.com>
2026-03-18 20:05:08 +0000

    feat: Add DeepReadonlyOptions to DeepReadonly (#467)
    * feat: add DeepReadonlyOptions
    
    * test: DeepReadonlyOptions in DeepReadonly
    
    * docs: DeepReadonly
    
    * docs: update README
    
    * docs: changeset

--- lib/deep-readonly/index.ts
+++ lib/deep-readonly/index.ts
@@ -1,39 +1,84 @@
 import { AnyArray } from "../any-array";
-import { Builtin } from "../built-in";
+import { CreateTypeOptions } from "../create-type-options";
 import { IsNever } from "../is-never";
 import { IsTuple } from "../is-tuple";
 import { IsUnknown } from "../is-unknown";
+import { Primitive } from "../primitive";
 
-type DeepReadonlyObject<Type> = {
-  readonly [Key in keyof Type]: Key extends typeof Symbol.iterator
-    ? Type[Key] extends () => Iterator<infer IteratorType, infer Return, infer Next>
-      ? () => Iterator<DeepReadonly<IteratorType>, DeepReadonly<Return>, DeepReadonly<Next>>
-      : DeepReadonly<Type[Key]>
-    : DeepReadonly<Type[Key]>;
+type DefaultDeepReadonlyOptions = {
+  builtin: {
+    date: false;
+    error: true;
+    regexp: false;
+  };
 };
 
-export type DeepReadonly<Type> = Type extends Exclude<Builtin, Error>
+type DeepReadonlyOptions = {
+  builtin: {
+    date: boolean;
+    error: boolean;
+    regexp: boolean;
+  };
+};
+
+type ConditionalBuiltin<Options extends Required<DeepReadonlyOptions>> =
+  | Primitive
+  | Function
+  | (Options["builtin"]["error"] extends false ? Error : never)
+  | (Options["builtin"]["date"] extends false ? Date : never)
+  | (Options["builtin"]["regexp"] extends false ? RegExp : never);
+
+type DeepReadonlyObjectWithOptions<Type, Options extends Required<DeepReadonlyOptions>> = {
+  readonly [Key in keyof Type]: Key extends typeof Symbol.iterator
+    ? Type[Key] extends () => Iterator<infer IteratorType, infer Return, infer Next>
+      ? () => Iterator<
+          DeepReadonlyWithOptions<IteratorType, Options>,
+          DeepReadonlyWithOptions<Return, Options>,
+          DeepReadonlyWithOptions<Next, Options>
+        >
+      : DeepReadonlyWithOptions<Type[Key], Options>
+    : DeepReadonlyWithOptions<Type[Key], Options>;
+};
+
+type DeepReadonlyWithOptions<
+  Type,
+  Options extends Required<DeepReadonlyOptions>,
+> = Type extends ConditionalBuiltin<Options>
   ? Type
   : Type extends Map<infer Keys, infer Values>
-  ? ReadonlyMap<DeepReadonly<Keys>, DeepReadonly<Values>>
+  ? ReadonlyMap<DeepReadonlyWithOptions<Keys, Options>, DeepReadonlyWithOptions<Values, Options>>
   : Type extends ReadonlyMap<infer Keys, infer Values>
-  ? ReadonlyMap<DeepReadonly<Keys>, DeepReadonly<Values>>
+  ? ReadonlyMap<DeepReadonlyWithOptions<Keys, Options>, DeepReadonlyWithOptions<Values, Options>>
   : Type extends WeakMap<infer Keys, infer Values>
-  ? WeakMap<DeepReadonly<Keys>, DeepReadonly<Values>>
+  ? WeakMap<DeepReadonlyWithOptions<Keys, Options>, DeepReadonlyWithOptions<Values, Options>>
   : Type extends Set<infer Values>
-  ? ReadonlySet<DeepReadonly<Values>>
+  ? ReadonlySet<DeepReadonlyWithOptions<Values, Options>>
   : Type extends ReadonlySet<infer Values>
-  ? ReadonlySet<DeepReadonly<Values>>
+  ? ReadonlySet<DeepReadonlyWithOptions<Values, Options>>
   : Type extends WeakSet<infer Values>
-  ? WeakSet<DeepReadonly<Values>>
+  ? WeakSet<DeepReadonlyWithOptions<Values, Options>>
   : Type extends Promise<infer Value>
-  ? Promise<DeepReadonly<Value>>
+  ? Promise<DeepReadonlyWithOptions<Value, Options>>
   : Type extends AnyArray<infer Values>
   ? IsNever<IsTuple<Type>> extends false
-    ? DeepReadonlyObject<Type>
-    : ReadonlyArray<DeepReadonly<Values>>
+    ? DeepReadonlyObjectWithOptions<Type, Options>
+    : ReadonlyArray<DeepReadonlyWithOptions<Values, Options>>
   : Type extends {}
-  ? DeepReadonlyObject<Type>
+  ? DeepReadonlyObjectWithOptions<Type, Options>
   : IsUnknown<Type> extends true
   ? unknown
   : Readonly<Type>;
+
+export type DeepReadonly<
+  Type,
+  OverrideDeepReadonlyOptions extends { builtin?: Partial<DeepReadonlyOptions["builtin"]> } = {},
+> = DeepReadonlyWithOptions<
+  Type,
+  {
+    builtin: CreateTypeOptions<
+      DeepReadonlyOptions["builtin"],
+      OverrideDeepReadonlyOptions["builtin"] extends {} ? OverrideDeepReadonlyOptions["builtin"] : {},
+      DefaultDeepReadonlyOptions["builtin"]
+    >;
+  }
+>;
