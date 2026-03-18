e0970b334ca469db9c0eaa839081545710324ee5
Alexey Berezin <2991847+Beraliv@users.noreply.github.com>
2026-03-18 20:00:47 +0000

    Revert "feat: Add DeepReadonlyOptions to DeepReadonly (#465)" (#466)
    This reverts commit f2401b2972973abed655426779cbd93d27e267b3.

--- lib/deep-readonly/index.ts
+++ lib/deep-readonly/index.ts
@@ -1,84 +1,39 @@
 import { AnyArray } from "../any-array";
-import { CreateTypeOptions } from "../create-type-options";
+import { Builtin } from "../built-in";
 import { IsNever } from "../is-never";
 import { IsTuple } from "../is-tuple";
 import { IsUnknown } from "../is-unknown";
-import { Primitive } from "../primitive";
 
-type DefaultDeepReadonlyOptions = {
-  builtin: {
-    date: false;
-    error: true;
-    regexp: false;
-  };
-};
-
-type DeepReadonlyOptions = {
-  builtin: {
-    date: boolean;
-    error: boolean;
-    regexp: boolean;
-  };
-};
-
-type ConditionalBuiltin<Options extends Required<DeepReadonlyOptions>> =
-  | Primitive
-  | Function
-  | (Options["builtin"]["error"] extends false ? Error : never)
-  | (Options["builtin"]["date"] extends false ? Date : never)
-  | (Options["builtin"]["regexp"] extends false ? RegExp : never);
-
-type DeepReadonlyObjectWithOptions<Type, Options extends Required<DeepReadonlyOptions>> = {
+type DeepReadonlyObject<Type> = {
   readonly [Key in keyof Type]: Key extends typeof Symbol.iterator
     ? Type[Key] extends () => Iterator<infer IteratorType, infer Return, infer Next>
-      ? () => Iterator<
-          DeepReadonlyWithOptions<IteratorType, Options>,
-          DeepReadonlyWithOptions<Return, Options>,
-          DeepReadonlyWithOptions<Next, Options>
-        >
-      : DeepReadonlyWithOptions<Type[Key], Options>
-    : DeepReadonlyWithOptions<Type[Key], Options>;
+      ? () => Iterator<DeepReadonly<IteratorType>, DeepReadonly<Return>, DeepReadonly<Next>>
+      : DeepReadonly<Type[Key]>
+    : DeepReadonly<Type[Key]>;
 };
 
-type DeepReadonlyWithOptions<
-  Type,
-  Options extends Required<DeepReadonlyOptions>,
-> = Type extends ConditionalBuiltin<Options>
+export type DeepReadonly<Type> = Type extends Exclude<Builtin, Error>
   ? Type
   : Type extends Map<infer Keys, infer Values>
-  ? ReadonlyMap<DeepReadonlyWithOptions<Keys, Options>, DeepReadonlyWithOptions<Values, Options>>
+  ? ReadonlyMap<DeepReadonly<Keys>, DeepReadonly<Values>>
   : Type extends ReadonlyMap<infer Keys, infer Values>
-  ? ReadonlyMap<DeepReadonlyWithOptions<Keys, Options>, DeepReadonlyWithOptions<Values, Options>>
+  ? ReadonlyMap<DeepReadonly<Keys>, DeepReadonly<Values>>
   : Type extends WeakMap<infer Keys, infer Values>
-  ? WeakMap<DeepReadonlyWithOptions<Keys, Options>, DeepReadonlyWithOptions<Values, Options>>
+  ? WeakMap<DeepReadonly<Keys>, DeepReadonly<Values>>
   : Type extends Set<infer Values>
-  ? ReadonlySet<DeepReadonlyWithOptions<Values, Options>>
+  ? ReadonlySet<DeepReadonly<Values>>
   : Type extends ReadonlySet<infer Values>
-  ? ReadonlySet<DeepReadonlyWithOptions<Values, Options>>
+  ? ReadonlySet<DeepReadonly<Values>>
   : Type extends WeakSet<infer Values>
-  ? WeakSet<DeepReadonlyWithOptions<Values, Options>>
+  ? WeakSet<DeepReadonly<Values>>
   : Type extends Promise<infer Value>
-  ? Promise<DeepReadonlyWithOptions<Value, Options>>
+  ? Promise<DeepReadonly<Value>>
   : Type extends AnyArray<infer Values>
   ? IsNever<IsTuple<Type>> extends false
-    ? DeepReadonlyObjectWithOptions<Type, Options>
-    : ReadonlyArray<DeepReadonlyWithOptions<Values, Options>>
+    ? DeepReadonlyObject<Type>
+    : ReadonlyArray<DeepReadonly<Values>>
   : Type extends {}
-  ? DeepReadonlyObjectWithOptions<Type, Options>
+  ? DeepReadonlyObject<Type>
   : IsUnknown<Type> extends true
   ? unknown
   : Readonly<Type>;
-
-export type DeepReadonly<
-  Type,
-  OverrideDeepReadonlyOptions extends { builtin?: Partial<DeepReadonlyOptions["builtin"]> } = {},
-> = DeepReadonlyWithOptions<
-  Type,
-  {
-    builtin: CreateTypeOptions<
-      DeepReadonlyOptions["builtin"],
-      OverrideDeepReadonlyOptions["builtin"] extends {} ? OverrideDeepReadonlyOptions["builtin"] : {},
-      DefaultDeepReadonlyOptions["builtin"]
-    >;
-  }
->;
